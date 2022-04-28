d18e2bb5b880caa44d66e6eb20754961c60459ac
AndreaMoscoso1 <104097500+AndreaMoscoso1@users.noreply.github.com>
2022-04-27 21:45:53 -0600

    Create totito.py

---
+++ totito.py
@@ -0,0 +1,53 @@
+from operator import truediv
+
+
+def create_empty_board():
+
+    board = []
+    for i in range(3):
+        board.append([None] * 3)
+
+    return board
+
+
+def print_board(board):
+
+    print('\n'.join([str(row) for row in board]))
+
+
+def update_board(board, position, player):
+    board[position[0]][position[1]]=player
+    return board
+
+
+def check_for_winner(board, player):
+    if (board[0][0]) == player and board[0][1] == player and board[0][2]== player):
+        return True
+    elif (board[1][0] == player and board[1][1] == player and board [1][2] == player):
+        return True
+    elif (board[2][0] == player and board[2][1] == player and board [2][2] == player):
+        return True
+    elif (board[0][0] == player and board[1][0] == player and board [2][0] == player):
+        return True
+    elif (board[0][1] == player and board[1][1] == player and board [2][1] == player):
+        return True
+    elif (board[0][2] == player and board[1][2] == player and board [2][2] == player):
+        return True
+    elif (board[0][0] == player and board[1][1] == player and board [2][2] == player):
+        return True
+    elif (board[0][2] == player and board[1][1] == player and board [2][0] == player):
+        return True
+    else:
+        return False
+
+
+
+'''
+Testing: 
+'''
+board = create_empty_board()
+print_board(board)
+update_board(board, [1,1], "0")
+update_board(board, [2,0], "x")
+update_board(board, [0,2], "x")
+print
